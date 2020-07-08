0fad843434c71998687b7d0a6a05114f3a9667f3
小哥 <48307703+strong-xiaoge@users.noreply.github.com>
2020-07-08 11:16:26 +0800

    Add files via upload

---
+++ Log系统/log.cpp
@@ -0,0 +1,242 @@
+#include "log.h"
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include <unistd.h>
+#include<time.h>
+#include<stdarg.h>
+
+namespace log{
+    /**
+     * @brief 获取系统当前时间
+    */
+    char*getTime(void){
+        time_t t = time(NULL);
+        struct tm*time = localtime(&t);
+        char*time_s=nullptr;
+        if(time){
+            time_s = asctime(time);
+            int size=strlen(time_s);
+            time_s[size-1]='\0';
+            time->tm_year += 1900;
+            time->tm_mon += 1;
+        }
+        return time_s;
+    }
+    /**
+     * @brief 数据输出函数
+    */
+    bool outPut(const APPENDER::OUTDATA*out){
+        FILE *fd = fopen(out->goalname.c_str(), "a");
+        if(!fd)return false;
+        if(fwrite(out->data, sizeof(char),out->len,fd)!=out->len)return false;
+        if(fclose(fd)==EOF)return false;
+        return true;
+    }
+    /**
+     * @brief 数据格式化函数
+    */
+    std::string defForMat(FORMAT::LOGLEVEL level, std::string log){
+        std::string str="[";
+        switch(level){
+            case FORMAT::INFO:str+="INFO";break;
+            case FORMAT::ERROR:str+="ERROR";break;
+            case FORMAT::WARNING:str+="WARN";break;
+            case FORMAT::DEBUG:str+="DEBUG";break;
+            case FORMAT::TRACE:str+="TRACE";break;
+            default:str+="UNKNOWN";break;
+        }
+        str+="]";
+        char*time=getTime();    //获取当前的系统时间
+        str = str+" ["+time+"]";
+        str = str+" : "+log+'\n';
+        return str;
+    }
+
+    FORMAT::FORMAT(LOGLEVEL level=UNKNOWN){
+        this->level=level;
+        this->format=defForMat;
+        fmtModflag=false;
+    }
+    /**
+     * @brief 格式化日志信息
+    */
+    std::string FORMAT::forMat(LOGLEVEL level, std::string log){
+        if(log.empty())return "";
+        fmtModflag=true;
+        std::string (*format)(FORMAT::LOGLEVEL, std::string)=this->format;
+        fmtModflag=false;
+        return format(level,log);
+    }
+    /**
+     * @brief 设置格式化实现实例
+    */
+    bool FORMAT::setForMat(std::string (*format)(FORMAT::LOGLEVEL, std::string)){
+        if(!format)return false;
+        while(fmtModflag);//增加线程安全
+        this->format=format;
+        return true;
+    }
+    APPENDER::APPENDER(FORMAT::LOGLEVEL level=FORMAT::UNKNOWN,
+                        bool (*output)(const OUTDATA*)=outPut,
+                        std::string name="LOG.log"):FORMAT(level){
+        filename=name;
+        this->Output=output;
+        outModflag=false;
+        //开辟后端缓冲区
+        outputbuffer = new char[buffersize];
+    }
+    APPENDER::~APPENDER()
+    {
+        if(this->bufferfullsize)append();
+        if(outputbuffer){
+            delete[] outputbuffer;
+            outputbuffer=nullptr;
+        }
+    }
+    /**
+     * @brief 信息输出实现
+    */
+    void APPENDER::append(void){
+        if(this->bufferfullsize){
+             OUTDATA out={
+                .data=this->outputbuffer,
+                .len=this->bufferfullsize,
+                .goalname=this->filename,
+                .level=this->getLevel()
+            };
+            outModflag=true;
+            bool (*output)(const OUTDATA*)= this->Output;
+            outModflag=false;
+            if(!output(&out))std::cerr<<"log out fail..."<<std::endl;
+            this->bufferfullsize=0;
+        }
+    }
+    /**
+     * @brief 设置输出实现实例
+    */
+    bool APPENDER::setOutput(bool (*output)(const log::APPENDER::OUTDATA *)){
+        if(!output)return false;
+        while(outModflag);//增加线程安全
+        this->Output=output;
+        return true;
+    }
+    LOG::LOG(FORMAT::LOGLEVEL level=FORMAT::UNKNOWN,
+            std::string name="LOG.log",
+            bool (*output)(const OUTDATA*)=outPut)
+            :APPENDER(level,output,name){
+        logbuffer = new char[buffersize];   //开辟前端缓冲区
+
+        backstageexit=false;
+        logupdataflag=false;
+        bufferfullsize=0;
+        //开辟缓冲区锁
+        buffermutex = new std::mutex;
+        //开辟后台记录线程
+        backstagethread = new std::thread(&LOG::logOutput,this);   
+    }
+    LOG::~LOG(){
+        if(this->backstagethread){
+            //保存未更新的数据
+            if(this->bufferfullsize)this->logUpdata();
+            //发送后台结束指令
+            backstageexit=true;
+            //等待后台任务退出
+            this->backstagethread->join();
+            delete this->backstagethread;
+            this->backstagethread=nullptr;
+        }
+        if(this->buffermutex){
+            delete this->buffermutex;
+            this->buffermutex=nullptr;
+        }
+        if(logbuffer){
+            delete[] logbuffer;
+            logbuffer=nullptr;
+        }
+    }
+    /**
+     * @brief 重载<<运算符
+    */
+    LOG& LOG::operator<<(const char*rvalue){
+        if(rvalue){
+            std::string log=this->FORMAT::forMat(FORMAT::getLevel(),rvalue);
+            const uint32_t size = log.length();
+            if(size > this->buffersize - this->bufferfullsize){
+                /*前端缓冲区已满*/
+                for(int i=0;i<100;++i){
+                    if(weitlogUpdate(0xFFFF))break;
+                }
+            }
+            buffermutex->lock();
+            memcpy(this->logbuffer+this->bufferfullsize,log.c_str(),size);
+            buffermutex->unlock();
+            this->bufferfullsize += size;
+        }
+        return *this;
+    }
+    /**
+     * @brief 重载printf输出
+    */
+    int LOG::printf(const char *fmt, ...){
+        va_list args;
+        va_start(args, fmt);
+        int size=vsnprintf(&logbuffer[bufferfullsize],buffersize-bufferfullsize,fmt,args);
+        if(size>0)bufferfullsize+=size;
+        va_end(args);
+        return size;
+    }
+    /**
+     * @brief 后台日志更新完成
+    */
+    void LOG::logUpdata(){
+        logupdataflag=true;
+    }
+    /**
+     * @brief 等待后台日志更新完成
+    */
+    bool LOG::weitlogUpdate(uint32_t outtime=0XFFFF){
+        this->logUpdata();
+        while(outtime--){
+            if(!logupdataflag)return true;
+        }
+        return false;
+    }
+    /**
+     * @brief 更新日志信息
+    */
+    void LOG::logOutput(void){
+        while(!backstageexit||logupdataflag){
+            if(this->APPENDER::bufferfullsize){
+                /*先将后台未及时记录的数据保存*/
+                buffermutex->lock();
+                this->APPENDER::append();
+                buffermutex->unlock();
+            }
+            if(this->bufferfullsize){
+                //交换前后缓冲区
+                std::swap(this->APPENDER::outputbuffer,this->logbuffer);
+                this->APPENDER::bufferfullsize=this->bufferfullsize;
+                this->bufferfullsize=0;
+                /*后台开始记录信息*/
+                buffermutex->lock();
+                this->APPENDER::append();
+                buffermutex->unlock();
+            }
+            logupdataflag=false;
+            for(int i=0;i<1000*3;++i){
+                if(logupdataflag)break;
+                if(!this->bufferfullsize&&backstageexit)return;
+                usleep(1000);
+            }
+        }
+    }
+}
+#ifdef TEST
+    int main(){
+        log::LOG info(log::FORMAT::INFO,"info.log");
+        info<<"this is test code ...";
+        info<<"the test code end ...";
+        info.printf("test %d ...\n",1);
+    }
+#endif
---
+++ Log系统/log.h
@@ -0,0 +1,133 @@
+#pragma one
+#include<string>
+#include<thread>
+#include<mutex>
+#include <atomic>
+
+namespace log{
+    /*缓冲区大小*/
+    #define BUFFSIZE 1024*4 //16kb
+    /**
+     * @brief 日志格式
+     * */
+    class FORMAT
+    {
+    public:
+    /*日志级别枚举*/
+        enum LOGLEVEL{
+            UNKNOWN=0,
+            INFO=1,
+            ERROR=2,
+            WARNING=3,
+            DEBUG=4,
+            TRACE=5,
+        };
+        FORMAT(LOGLEVEL);
+        void setLevel(LOGLEVEL level){
+            if(level<UNKNOWN && level>TRACE)return;
+            this->level=level;
+        }
+        LOGLEVEL getLevel(void){
+            return this->level;
+        }
+        std::string forMat(LOGLEVEL,std::string);//格式化日志消息
+        bool setForMat(std::string (*format)(FORMAT::LOGLEVEL, std::string));
+    private:
+        std::atomic<bool> fmtModflag;
+        std::string (*format)(FORMAT::LOGLEVEL, std::string); 
+        LOGLEVEL level=UNKNOWN;
+    };
+    /** 
+     * @brief 日志输出
+    */
+    class APPENDER:public FORMAT
+    {
+    public:
+        /*输出数据*/
+        typedef struct{
+            const char*data;
+            const uint32_t len;
+            const std::string goalname;
+            const FORMAT::LOGLEVEL level;
+        }OUTDATA;
+        APPENDER(FORMAT::LOGLEVEL level,
+                bool (*output)(const OUTDATA*),
+                std::string name);
+        ~APPENDER();
+        void setLogNmae(std::string name) { 
+            filename=name;
+        }
+        std::string getLogNmae(void){
+            return filename;
+        }
+        void append(void);   //信息输出
+        bool setOutput(bool(*output)(const OUTDATA*));
+    private:
+        const uint32_t buffersize=BUFFSIZE;
+        std::atomic<bool> outModflag;
+        std::string filename;//日志输出文件名
+        bool (*Output)(const OUTDATA*);
+    protected:
+        char*outputbuffer=nullptr;  //输出缓冲区
+        uint32_t bufferfullsize=0;  //缓冲区记录字节数
+    };
+    /**
+     * @brief 日志器
+    */
+    class LOG:public APPENDER
+    {
+    public:
+        LOG(FORMAT::LOGLEVEL level,
+            std::string name,
+            bool (*output)(const OUTDATA*));
+        ~LOG();
+        //后台日志更新完成
+        void logUpdata(void);
+        //等待后台数据更新完成
+        bool weitlogUpdate(uint32_t outtime);
+        //重载printf
+        int printf(const char*fmt,...);
+        //重载<<运算符
+        LOG& operator <<(const char*);    
+        LOG& operator <<(const char rvalue){
+            return *this<<&rvalue;
+        }
+        LOG& operator <<(const short rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const int rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const long rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const float rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const double rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const unsigned char rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const unsigned short rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const unsigned int rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+        LOG& operator <<(const unsigned long rvalue){
+            return *this<<std::to_string(rvalue).c_str();
+        }
+    private:
+        const uint32_t buffersize=BUFFSIZE;
+        std::atomic<uint32_t> bufferfullsize;  //缓冲区记录字节数
+        char*logbuffer=nullptr;     //日志缓冲区
+        std::mutex*buffermutex=nullptr;     //缓冲区锁
+
+        void logOutput(void);   //后台信息更新
+        std::thread*backstagethread=nullptr; //后台线程
+        std::atomic<bool> backstageexit;   //后台记录任务退出标志
+        std::atomic<bool> logupdataflag; //数据更新标志位(原子锁)
+    };
+}
